e59484767a62c4779b5be8ba267b9d096154b9ca
rifqee23 <rifqifebrianto746@gmail.com>
2024-07-07 11:33:04 +0700

    mldeks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-   
+    container: {
+      center: true
+    },
     extend: {
     
        backgroundImage: {
         'gradient-three-colors': 'linear-gradient(to right, #F72585, #5F69D3, #7209B7)',
+        'gradient-heading' :  'linear-gradient(to right, #7209B7, #3A0CA3)',
        },
        backgroundColor: {
         'custom-gray': 'rgba(217, 217, 217, 0.2)',
@@ -17,6 +20,14 @@ module.exports = {
        },
        boxShadow: {
         'custom' : '1px 1px 40px rgba(0,0,0,0.2), 0 -1px 10px rgba(0,0,0,0.1)',
+       },
+       fontSize: {
+        'heading' : '2rem',
+        'sub-heading' : '1.75rem'
+
+       },
+       textColor: {
+        'primary' : '#132D46'
        }
     },
   },
